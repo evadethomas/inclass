12939ed5f947e394eeba8b10b6a16ae33a2b6247
ejung-usf <ejung@cs.usfca.edu>
2023-01-31 10:22:05 -0800

    argument and pointer demo

--- week02/section01/arg_test.c
+++ week02/section01/arg_test.c
@@ -1,12 +1,33 @@
 #include <stdio.h> 
 #include <string.h>
 
+// @EJ 2023-01-31
+// prints the arguments one char at a time
+
 int main(int argc, char** argv) {
-	//printf("%d\n", argc);
+
+	/* method 1: treating char** as an array of strings
+	 * and treating char* as an array of characters
+	 */
+
+	for (int i=0; i<argc; i++) {
+		char* str = argv[i];
+		for (int j=0; j<strlen(str); j++) {
+			printf("%c\n", str[j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	//method 2: using pointer arithmetic 
+
 	for (int i=0; i<argc; i++) {
 		char* str = argv[i];
 		for (int j=0; j<strlen(str); j++) {
 			printf("%c\n", *(*(argv+i) + j));
 		}
+		printf("\n");
 	}
+
+	return 0;
 }
