552f0adf124cc580da544fac0a151983430db58c
ejung-usf <ejung@cs.usfca.edu>
2023-02-15 14:21:12 -0800

    struct passing example

---
+++ week04/section01/structpassing.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct point {
+    float x;
+    float y;
+};
+
+void y_flip(struct point p) {
+    p.y = -p.y;
+}
+
+void ptr_y_flip(struct point *p_) {
+    struct point p = *p_;
+    p.y = -p.y;
+    printf("in flip: %f, %f\n", p.x, p.y);
+}
+
+int main(int argc, char* argv[]) {
+    struct point p;
+    p.x = 1.0;
+    p.y = 0.1;
+    printf("before flip: %f, %f\n", p.x, p.y);
+    y_flip(p);
+    printf("after flip: %f, %f\n", p.x, p.y);
+    ptr_y_flip(&p);
+    printf("after flip: %f, %f\n", p.x, p.y);
+}
+
