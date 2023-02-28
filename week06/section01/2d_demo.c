0befcc2052b2f1fdd255d5940b97dd5700610dfa
ejung-usf <ejung@cs.usfca.edu>
2023-02-28 10:42:00 -0800

    pointer demo

--- week06/section01/2d_demo.c
+++ week06/section01/2d_demo.c
@@ -10,7 +10,9 @@ int main(int argc, char* argv[]) {
 
     int board_size = atoi(argv[1]);
     int board[board_size][board_size];
-
+    //board is type int**
+    //board[0] is type int*
+    //board[0][0] is type int
     for (int r = 0; r < board_size; r++) {
         for (int c = 0; c < board_size; c++) {
             board[r][c] = r * board_size + c + 1;
@@ -19,14 +21,17 @@ int main(int argc, char* argv[]) {
  
     for (int r = 0; r < board_size; r++) {
         for (int c = 0; c < board_size; c++) {
-            printf("%p %2d ", &board[r][c], board[r][c]);
+            printf("%p %d ", &board[r][c], board[r][c]);
         }
         printf("\n");
     }
     printf("\n");
- 
+ /*
     for (int r = 0; r < board_size; r++) {
-        int* row = *(board+r);
+        //int* row = *(board+r); 
+        int* row = board[r];
+        //this is wrong: int* column = board[c];
+        // *(board+r) = board[r] = the int array for each row r
         for (int c = 0; c < board_size; c++) {
             printf("%p %2d ", &row[c], row[c]);
         }
@@ -41,12 +46,20 @@ int main(int argc, char* argv[]) {
         }
         printf("\n");
     }
-    printf("\n");
-    
+    printf("\n");*/ 
+    printf("%p %d\n", board+2, **(board+2));  
+    printf("%p %d\n", *board, *(*board));  
+
+    printf("%p %d\n", *(board+1)+5, *(*(board+1)+5));  
+    printf("%p %d\n", board[1]+5, *(board[1]+5));  
+    printf("%p %d\n", *(board+8), **(board+8));  
+
+    /*
     for (int r = 0; r < board_size * board_size; r++) {
         printf("%p %2d ", (*board)+r, *((*board)+r));
         if (((r+1) % board_size) == 0) {
             printf("\n");
         }
-    }
+    }*/
+    
 }
